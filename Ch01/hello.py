b7c7db81a73d2e522ecfd92d35edbec431740b7e
alswns3906 <alswns3906@naver.com>
2023-01-03 13:47:14 +0900

    2023/01/03

---
+++ Ch01/hello.py
@@ -0,0 +1,10 @@
+
+"""
+날짜 : 2023/01/03
+이름 : 홍민준
+내용 : Python Hello World
+"""
+
+print('Hello World')
+print('Hello Python')
+print("안녕하세요. 파이썬")
---
+++ Ch02/1.변수.py
@@ -0,0 +1,18 @@
+
+"""
+날짜 : 2023/01/03
+이름 : 홍민준
+내용 : 파이썬 변수 실습하기
+"""
+
+var1 = 1
+var2 = 2
+
+var3 = var1 + var2
+print('var3 : ', var3)
+
+var1 = 10
+var2 = 20
+
+var3 = var1 + var2
+print('var3 : ', var3)
